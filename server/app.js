f94f794a4ea3024d6456232ab48b08d972e5837c
naveenankireddy <naveenkumarreddyankireddy@gmail.com>
2020-11-24 15:52:57 +0530

    initial commit

---
+++ server/app.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.listen(PORT, () => {
+  console.log("Server is started at", PORT, "CC dug dug dug dug dug.......");
+});
